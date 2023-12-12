47aefece4072f951fa65e7e750a47463f08e3dc0
Mohamed Bakoush <bakoushmohamed@gmail.com>
2023-12-12 18:49:05 +0100

    NUXT Framework

---
+++ FRONT-END/app.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    import "@/index.css"; 
+
+    useHead({
+        title: 'PROCESS-VIDEO-XXX',
+        meta: [
+            { name: 'description', content: "Embark on a PROCESS-VIDEO-XXX journey, where our passion lies in transforming challenges into creative explorations." },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        ],
+        
+    });
+
+</script>
+
+<template>
+    <NuxtLayout>
+        <NuxtPage/>
+    </NuxtLayout> 
+</template>
